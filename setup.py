2027565efb88720e0fb79af54e8d4df99d32d631
Ilan Schnell <ischnell@enthought.com>
2012-02-22 15:32:53 -0600

    cleanup

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-#------------------------------------------------------------------------------
-#  Copyright (c) 2011, Enthought, Inc.
-#  All rights reserved.
-#------------------------------------------------------------------------------
 # Copyright (c) 2011 by Enthought, Inc.
 # All rights reserved.
 
 from setuptools import setup, find_packages
 
-setup(name='encore',
+
+setup(
+    name='encore',
     version='0.1',
     author='Enthought, Inc',
     author_email='info@enthought.com',
