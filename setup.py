4dc504f11c6e22a98b9cb02674e24eae799753c7
Ilan Schnell <ischnell@enthought.com>
2012-02-22 15:36:33 -0600

    REL: master now 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='encore',
-    version='0.1',
+    version='0.1.1',
     author='Enthought, Inc',
     author_email='info@enthought.com',
     url='https://github.com/enthought/encore',
