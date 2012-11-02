42659e3f17aa77a216b1815ee95f7b78247b63c8
Corran Webster <cwebster@enthought.com>
2012-11-02 15:04:27 -0500

    An eager synchronous executor compatible with concurrent.futures in Python 3.3 and the backport to Python 2.7.

---
+++ encore/concurrent/futures/__init__.py
@@ -0,0 +1,9 @@
+#
+# (C) Copyright 2012 Enthought, Inc., Austin, TX
+# All right reserved.
+#
+# This file is open source software distributed according to the terms in
+# LICENSE.txt
+#
+
+from encore.concurrent.futures.synchonous import SynchronousExecutor
