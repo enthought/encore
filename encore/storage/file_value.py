45c38257b5cd1246280cafdc174b1d1b3bb6015b
Corran Webster <cwebster@enthought.com>
2012-11-05 14:40:23 -0600

    Better no implementation than a bad implementation.
    Unsure of behaviour on windows.

--- encore/storage/file_value.py
+++ encore/storage/file_value.py
@@ -6,9 +6,8 @@
 #
 
 import os
-import stat
 
-from .abstract_store import Value
+from .abstract_store import Value, AuthorizationError
 
 class FileValue(Value):
     
@@ -30,38 +29,12 @@ class FileValue(Value):
 
     @property
     def permissions(self):
-        tags = {
-            self._owner: {
-                'read': stat.S_IRUSR,
-                'write': stat.S_IWUSR,
-                'execute': stat.S_IXUSR,
-            },
-            'group': {
-                'read': stat.S_IRGRP,
-                'write': stat.S_IWGRP,
-                'execute': stat.S_IXGRP,
-            },
-            'other': {
-                'read': stat.S_IROTH,
-                'write': stat.S_IWOTH,
-                'execute': stat.S_IXOTH,
-            }
-        }
-        return {'owned': {self._owner},
-            'read': {tag for tag, masks in tags.items() if masks['read'] &
-                self._mode},
-            'write': {tag for tag, masks in tags.items() if masks['write'] &
-                self._mode},
-            'execute': {tag for tag, masks in tags.items() if masks['execute'] &
-                self._mode},
-        }
+        raise AuthorizationError("key not owned by user")
         
     def _stat(self):
         stat = os.stat(self._path)
         self.size = stat.st_size
         self.created = None
         self.modified = stat.st_mtime
-        self._mode = stat.st_mode
-        self._owner = str(stat.st_uid)
             
             
