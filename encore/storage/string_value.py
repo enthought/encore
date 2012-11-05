45595597a0661eae27ad346ec5135f37342ef712
Corran Webster <cwebster@enthought.com>
2012-11-05 16:25:03 -0600

    Add permissions to StringValue.

--- encore/storage/string_value.py
+++ encore/storage/string_value.py
@@ -12,12 +12,14 @@ from .abstract_store import Value, AuthorizationError
 
 class StringValue(Value):
     
-    def __init__(self, data='', metadata=None, created=None, modified=None):
+    def __init__(self, data='', metadata=None, permissions=None, created=None,
+            modified=None):
         if not isinstance(data, basestring):
             raise ValueError(data)
         self._data = data
         self._data_stream = None
         self._metadata = metadata if metadata is not None else {}
+        self._permissions = permissions
         self.size = len(self._data)
         self.created = created if created is not None else time.time()
         self.modified = modified if modified is not None else time.time()
@@ -34,5 +36,7 @@ class StringValue(Value):
 
     @property
     def permissions(self):
-        raise AuthorizationError("key not owned by user")
+        if self._permissions is None:
+            raise AuthorizationError("key not owned by user")
+        return self._permissions
         
