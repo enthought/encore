62435018bf732f0755197cb0b4f075d260d116ee
Corran Webster <cwebster@enthought.com>
2012-11-05 11:07:21 -0600

    Remove api.py

--- encore/concurrent/futures/api.py
+++
@@ -1,9 +0,0 @@
-#
-# (C) Copyright 2012 Enthought, Inc., Austin, TX
-# All right reserved.
-#
-# This file is open source software distributed according to the terms in
-# LICENSE.txt
-#
-
-from .synchronous import SynchronousExecutor
