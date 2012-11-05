d5c017389d0a3713fefa0b8fa6b870370e5fc1ca
Corran Webster <cwebster@enthought.com>
2012-11-05 15:05:17 -0600

    Add error handling.

--- encore/storage/dynamic_url_store.py
+++ encore/storage/dynamic_url_store.py
@@ -10,7 +10,7 @@ import json
 import urllib
 import rfc822
 
-from .abstract_store import AbstractStore, Value
+from .abstract_store import AbstractAuthorizingStore, Value, AuthorizationError
 from .utils import DummyTransactionContext
 
 class RequestsURLValue(Value):
@@ -22,14 +22,15 @@ class RequestsURLValue(Value):
         self._key = key
         self._url_format = url_format
         self._parts = parts if parts is not None else {'data': 'data',
-            'metadata': 'metadata'}
+            'metadata': 'metadata', 'permissions': 'auth'}
         self._data_response = None
         self._size = None
         self._created = None
         self._modified = None
     
     def _url(self, part):
-        return self._url_format.format(base=self._base_url, key=key, part=part)
+        return self._url_format.format(base=self._base_url, key=self._key,
+            part=part)
     
     @property
     def data(self):
@@ -41,8 +42,27 @@ class RequestsURLValue(Value):
     def metadata(self):
         headers = {'Accept': 'application/json'}
         response = self._session.get(self._url('metadata'), headers=headers)
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+
         metadata = json.loads(response.text)
         return metadata
+        
+    @property
+    def permissions(self):
+        headers = {'Accept': 'application/json'}
+        response = self._session.get(self._url('permissions'), headers=headers)
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+
+        permissions = json.loads(response.text)
+        return permissions
         
     @property
     def size(self):
@@ -59,6 +79,11 @@ class RequestsURLValue(Value):
     def open(self):
         self._data_response = self._session.get(self._url('data'),
             prefetch=False)
+        if self._data_response.status_code == 404:
+            raise KeyError(self._key)
+        elif self._data_response.status_code == 403:
+            raise AuthorizationError(self._key)
+        self._data_response.raise_for_status()
             
         size = self._data_response.headers['Content-Length']
         if size is not None:
@@ -73,16 +98,16 @@ class RequestsURLValue(Value):
         return self._data_response.raw
         
 
-class DynamicURLStore(AbstractStore):
+class DynamicURLStore(AbstractAuthorizingStore):
     
     def __init__(self, base_url, query_url, url_format='{base}/{key}/{part}',
             parts=None):
-        super(AbstractStore, self).__init__()
+        super(AbstractAuthorizingStore, self).__init__()
         self.base_url = base_url
         self.query_url = query_url
         self.url_format = url_format
         self.parts = parts if parts is not None else {'data': 'data',
-            'metadata': 'metadata'}
+            'metadata': 'metadata', 'permissions': 'auth'}
     
     def connect(self, credentials=None):
         """ Connect to a DynamicURLStore
@@ -105,32 +130,64 @@ class DynamicURLStore(AbstractStore):
         safe_key = urllib.quote(key, safe="/~!$&'()*+,;=:@")
         return RequestsURLValue(self._session, self.base_url, safe_key, self.url_format,
             self.parts)
-    get.__doc__ = AbstractStore.get.__doc__
+    get.__doc__ = AbstractAuthorizingStore.get.__doc__
     
-    def set(key, value, buffer_size=1048576):
+    def set(self, key, value, buffer_size=1048576):
         super(DynamicURLStore, self).set(key, value, buffer_size)
-    set.__doc__ = AbstractStore.set.__doc__
+    set.__doc__ = AbstractAuthorizingStore.set.__doc__
  
     def delete(self, key):
         pass
-    delete.__doc__ = AbstractStore.delete.__doc__
+    delete.__doc__ = AbstractAuthorizingStore.delete.__doc__
    
     def set_data(self, key, data, buffer_size=1048576):
         response = self._session.put(self._url(key, 'data'), data)
-        # XXX check response!
-    set_data.__doc__ = AbstractStore.set_data.__doc__
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+    set_data.__doc__ = AbstractAuthorizingStore.set_data.__doc__
     
     def set_metadata(self, key, metadata):
         response = self._session.put(self._url(key, 'metadata'),
             json.dumps(metadata))
-        # XXX check response!
-    set_metadata.__doc__ = AbstractStore.set_metadata.__doc__
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+    set_metadata.__doc__ = AbstractAuthorizingStore.set_metadata.__doc__
     
     def update_metadata(self, key, metadata):
         response = self._session.post(self._url(key, 'metadata'),
             json.dumps(metadata))
-        # XXX check response!
-    update_metadata.__doc__ = AbstractStore.update_metadata.__doc__
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+    update_metadata.__doc__ = AbstractAuthorizingStore.update_metadata.__doc__
+
+    def set_permissions(self, key, permissions):
+        response = self._session.put(self._url(key, 'permissions'),
+            json.dumps(permissions))
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+    set_metadata.__doc__ = AbstractAuthorizingStore.set_metadata.__doc__
+    
+    def update_permissions(self, key, permissions):
+        response = self._session.post(self._url(key, 'permissions'),
+            json.dumps(permissions))
+        if response.status_code == 404:
+            raise KeyError(self._key)
+        elif response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
+    update_metadata.__doc__ = AbstractAuthorizingStore.update_metadata.__doc__
 
     def transaction(self, notes):
         """ Provide a transaction context manager
@@ -143,11 +200,14 @@ class DynamicURLStore(AbstractStore):
     def query(self, select=None, **kwargs):
         for key in self.query_keys(**kwargs):
             yield self.get_metadata(key, select=select)
-    query.__doc__ = AbstractStore.query.__doc__
+    query.__doc__ = AbstractAuthorizingStore.query.__doc__
 
     def query_keys(self, **kwargs):
         response = self._session.get(self.query_url, params=kwargs)
+        if response.status_code == 403:
+            raise AuthorizationError(self._key)
+        response.raise_for_status()
         for line in response.iter_lines():
             yield line
-    query_keys.__doc__ = AbstractStore.query_keys.__doc__
+    query_keys.__doc__ = AbstractAuthorizingStore.query_keys.__doc__
             
