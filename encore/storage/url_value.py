801354d3dedbef6875d9024b2481c1a5c59219aa
Didrik Pinte <dpinte@enthought.com>
2017-01-11 18:13:58 +0100

    FIX: python 3 compatibility for imports

--- encore/storage/url_value.py
+++ encore/storage/url_value.py
@@ -5,8 +5,8 @@
 # This file is open source software distributed according to the terms in LICENSE.txt
 #
 
-import urllib2
-import rfc822
+from six.moves import urllib
+from email.utils import parsedate_tz, mktime_tz
 
 from .abstract_store import Value, AuthorizationError
 from .utils import BufferIteratorIO, buffer_iterator
@@ -16,7 +16,7 @@ class URLValue(Value):
     def __init__(self, url, metadata=None, opener=None):
         self._url = url
         self._metadata = metadata if metadata is not None else {}
-        self._opener = opener if opener is not None else urllib2.urlopen
+        self._opener = opener if opener is not None else urllib.request.urlopen
         self._data_stream = None
         self._size = None
         self._created = None
@@ -58,7 +58,7 @@ class URLValue(Value):
         # need to build a reqquest with a range header
         start_string = str(start) if start is not None else ''
         end_string = str(end) if end is not None else ''
-        request = urllib2.Request(self._url, headers={
+        request = urllib.request.Request(self._url, headers={
             'Range': 'bytes={0}-{1}'.format(start_string, end_string),
         })
         stream = self._opener.open(request)
@@ -88,7 +88,7 @@ class URLValue(Value):
 
         modified = headers.get('Last-Modified', None)
         if modified is not None:
-            modified = rfc822.mktime_tz(rfc822.parsedate_tz(modified))
+            modified = mktime_tz(parsedate_tz(modified))
         self._modified = modified
 
         return self._data_stream
