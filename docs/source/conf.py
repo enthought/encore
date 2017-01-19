9b0c691a8cbebcc5c19520cf7a684c3094d51671
Corran Webster <cwebster@enthought.com>
2017-01-19 10:51:48 +0000

    Update documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Encore'
-copyright = u'2011-2013, Enthought, Inc.'
+copyright = u'2011-2017, Enthought, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
