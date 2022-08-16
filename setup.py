a475ec68d36eb7118be655afcfd9774dd4b4cf48
Mark Dickinson <mdickinson@enthought.com>
2022-08-16 16:36:44 +0100

    Update build machinery

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ from setuptools import setup, find_packages
 encore_init = os.path.join('encore', '__init__.py')
 d = runpy.run_path(encore_init)
 
+def get_long_description():
+    """ Read long description from README.txt. """
+    with open("README.rst", "r", encoding="utf-8") as readme:
+        return readme.read()
+
 setup(
     name='encore',
     version=d['__version__'],
@@ -16,7 +21,8 @@ setup(
     url='http://docs.enthought.com/encore/',
     download_url='https://github.com/enthought/encore',
     description='Low-level core modules for building Python applications',
-    long_description=open('README.rst').read(),
+    long_description=get_long_description(),
+    long_description_content_type="text/x-rst",
     packages=find_packages(),
     requires=[],
     python_requires=">=3.6",
